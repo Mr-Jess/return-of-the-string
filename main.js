3922583387de8d6514a3b1fe18da12e4ac46499e
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-09-26 15:31:18 -0400

    string practice

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-function capitalize() {
+function capitalize(str) {
+  return str.toUpperCase();
 }
 
-function exclaim() {
+function exclaim(str) {
+  return str + '!';
 }
 
-function firstCharacter() {
+function firstCharacter(str) {
+  return str[0];
 }
 
-function lastCharacter() {
+function lastCharacter(str) {
+  return str[str.length -1];
 }
 
-function oneCharacter() {
+function oneCharacter(str) {
+  return str[8];
 }
 
-function twoCharacters() {
+function twoCharacters(str) {
+  const answer = str[2] + str[11];
+  return answer;
 }
 
-function initials() {
+function initials(str) {
+  return firstCharacter(str) + '.' + str[str.indexOf(' ') + 1] + '.';
 }
 
-function yeller() {
+function yeller(str) {
+  // return capitalize(str) + '!!!';
+
+  const capitalized = capitalize(str);
+  const pun1 = exclaim(capitalized)
+  const pun2 = exclaim(pun1);
+  const pun3 = exclaim(pun2);
+  return pun3;
 }
 
 
